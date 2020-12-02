9738a646b2a7c2f6356eb74ba8c5bb7b8ff9e33c
takuma.ohashi <iroiroblog3japan@gmail.com>
2020-12-02 14:05:28 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,15 +15,21 @@ class TasksController < ApplicationController
       @task = Task.new(task_params)
 
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
+      flash[:success] = "Task が正常に投稿されました"
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
+      flash.now[:danger] = "Task が投稿されませんでした"
       render :new
     end
   end
 
   def edit
        @task = Task.find(params[:id])
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
