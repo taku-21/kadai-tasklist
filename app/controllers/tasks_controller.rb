8f67a6718bd83285bbed24f37d32945392f7f131
takuma.ohashi <iroiroblog3japan@gmail.com>
2020-12-02 13:04:26 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-      @tasks =Tasks.all
+      @tasks = Task.all
   end
 
   def show
@@ -25,25 +25,5 @@ class TasksController < ApplicationController
 
   def edit
        @task = Task.find(params[:id])
-  end
-
-  def update
-      @task = Task.find(params[:id])
-
-    if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Task は更新されませんでした'
-      render :edit
-    end
-  end
-
-  def destroy
-      @task = Task.find(params[:id])
-    @task.destroy
-
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
   end
 end
