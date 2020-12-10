adafab7f5ce063e792b1ca23e7aa876d8a782c9f
takuma.ohashi <iroiroblog3japan@gmail.com>
2020-12-10 06:52:20 +0000

    user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,7 +26,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: email)
     if @user && @user.authenticate(password)
       # ログイン成功
-      session[:user_id] = @user.id
+      session[:user_id] = @tasks
       return true
     else
       # ログイン失敗
