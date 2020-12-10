24970a3e94d1a7bb0633871afb15b5371a3d61a4
takuma.ohashi <iroiroblog3japan@gmail.com>
2020-12-10 13:50:15 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
    
    get "signup", to: "users#new"
    
-   resources :users, only: [:index, :show, :new, :create]
+   resources :tasks
+   resources :users, only: [:new, :create]
              
 end
