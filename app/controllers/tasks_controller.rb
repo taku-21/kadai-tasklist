0bd4f44a99d81c57d73c43d6cac42ce78131b897
takuma.ohashi <iroiroblog3japan@gmail.com>
2020-12-15 07:29:25 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,10 +48,6 @@ class TasksController < ApplicationController
 
   private
   
-  def set_task
-    @task= Task.find(params[:id])
-  end
-
   def task_params
     params.require(:task).permit(:content, :status)
   end
